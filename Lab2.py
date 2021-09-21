445bf55237a486ebf5a08775a5ec2879551141ec
Tumbokon, Alec Marcial M <alectumbokon@gmail.com>
2021-09-21 04:35:55 +0000

    1st commit

---
+++ Lab2.py
@@ -0,0 +1,14 @@
+print("Your Average Grade")
+print("Prelim Grade: ")
+pre = int(input())
+print("Midterm Grade: ")
+mid = int(input())
+print("Semi Final Grade: ")
+semi = int(input())
+print("Final Grade: ")
+fin = int(input))
+
+total= pre+mid+semi+fin
+ave= total/4
+
+print("Your Average is {}".format(ave))
