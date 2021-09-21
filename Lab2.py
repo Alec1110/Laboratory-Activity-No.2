35539ad120639475ecc2b7f4ee080bc576245530
Tumbokon, Alec Marcial M <alectumbokon@gmail.com>
2021-09-21 05:19:48 +0000

    3rd commit

--- Lab2.py
+++ Lab2.py
@@ -17,3 +17,20 @@ if ave >= 75:
     print("You Passed :D")
 else:
     print("You Failed :(")
+
+if ave >= 99 <=100:
+    print("Your Mark is A")
+elif ave >=90 <=99:
+    print("Your Mark is B")
+elif ave >=80 <=89:
+    print("Your Mark is C")
+elif ave >=71 <=79:
+    print("Your Mark is D")
+elif ave >=61 <=70:
+    print("Your Mark is E")
+elif ave >= 60:
+    print("Your Mark is F")
+else:
+    print("Invalid Input")
+
+
